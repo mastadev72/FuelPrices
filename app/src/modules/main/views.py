30a4b6fadbbb6fd9d9a633d5d639aa6badfedf20
Nikoloz Naskidashvili <nikoloz2005@gmail.com>
2022-08-21 21:32:23 +0400

    redactor(app): code refactor & minor fixes

--- app/src/modules/main/views.py
+++ app/src/modules/main/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template
 
 from src.modules.main.forms import ComparePriceForm
 from src.modules.main.models import FuelPriceModel
