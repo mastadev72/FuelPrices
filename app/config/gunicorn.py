bbfb27683d66bad1beb814a6bb58f10a15f7ac7f
Nikoloz Naskidashvili <nikoloz2005@gmail.com>
2022-09-05 16:56:39 +0400

    refactor: minor changes

---
+++ app/config/gunicorn.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import os
+from pathlib import Path
+
+from environs import Env
+
+BASE_DIR = Path(__file__).resolve().parent
+
+env = Env()
+env.read_env(os.path.join(BASE_DIR, '.env.prod'))  # Change to .env.prod in production
+
+bind = "0.0.0.0:" + env.str("PORT")
+workers = env.int("GUNICORN_WORKERS")
+worker_class = "gevent"
+timeout = 300
+max_requests = 1000000
+limit_request_line = 8190
+
+accesslog = "/var/log/gunicorn.access.log"
+errorlog = "/var/log/gunicorn.error.log"
