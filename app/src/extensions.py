f987bd32dbb12d1b9be11b50f086d2107619347d
Nikoloz Naskidashvili <nikoloz2005@gmail.com>
2022-09-26 00:36:28 +0400

    fix: remove Flask-Mail from extensions.py

--- app/src/extensions.py
+++ app/src/extensions.py
@@ -4,15 +4,13 @@ from flask_caching import Cache
 from flask_debugtoolbar import DebugToolbarExtension
 from flask_migrate import Migrate
 from flask_static_digest import FlaskStaticDigest
-from flask_mail import Mail
 
 from src.database import db
 
 migrate = Migrate()
-mail = Mail()
 cache = Cache()
 debug_toolbar = DebugToolbarExtension()
 flask_static_digest = FlaskStaticDigest()
 
-extensions = [db, mail, cache, debug_toolbar, flask_static_digest]
+extensions = [db, cache, debug_toolbar, flask_static_digest]
 extensions_with_db = [migrate]
