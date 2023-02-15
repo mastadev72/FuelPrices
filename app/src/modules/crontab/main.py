be0bb24adf9a79c048a4d349e6c33de52f140e04
Nikoloz Naskidashvili <nikoloz2005@gmail.com>
2023-02-15 18:58:15 +0400

    refactor: parser imports

--- app/src/modules/crontab/main.py
+++ app/src/modules/crontab/main.py
@@ -1,11 +1,13 @@
 import logging
 
 from src.extensions import db
-from src.modules.crontab.parsers.gulf_parser import parse_gulf_data
-from src.modules.crontab.parsers.rompetrol_parser import parse_rompetrol_data
-from src.modules.crontab.parsers.wissol_parser import parse_wissol_data
-from src.modules.crontab.parsers.lukoil_parser import parse_lukoil_data
-from src.modules.crontab.parsers.socar_parser import parse_socar_data
+from src.modules.crontab.parsers import (
+    parse_gulf_data,
+    parse_lukoil_data,
+    parse_rompetrol_data,
+    parse_socar_data,
+    parse_wissol_data,
+)
 from src.modules.crontab.services import parsed_data_confirmation, fill_db_with_parsed_data
 
 
---
+++ app/src/modules/crontab/parsers/__init__.py
@@ -0,0 +1,6 @@
+# flake8: noqa
+from .gulf_parser import parse_gulf_data
+from .lukoil_parser import parse_lukoil_data
+from .rompetrol_parser import parse_rompetrol_data
+from .socar_parser import parse_socar_data
+from .wissol_parser import parse_wissol_data
