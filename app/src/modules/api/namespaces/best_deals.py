317aeb1cb5a23a2744f6103567683d9ccff8eb16
Nikoloz Naskidashvili <nikoloz2005@gmail.com>
2023-02-22 23:35:03 +0400

    feat: add lowest price endpoint and other stuff
    - remove marshmallow schemas and move to restX native models

---
+++ app/src/modules/api/namespaces/best_deals.py
@@ -0,0 +1,36 @@
+from flask_restx import Resource, Namespace, fields
+
+from src.modules.main.services import get_lowest_current_prices
+
+api = Namespace('Lowest Deals')
+
+lowest_sr = api.model('Lowest', {
+    'fuel_type': fields.String(required=True, description="Fuel type"),
+    'price': fields.Float(required=True, description="Fuel price"),
+    'providers': fields.List(fields.String, required=True, description="Fuel Providers")
+})
+
+
+@api.route("/")
+class Lowest(Resource):
+    """All lowest prices API endpoint."""
+
+    @api.doc('get_lowest_prices')
+    @api.marshal_with(lowest_sr)
+    def get(self):
+        """
+        Get lowest prices for each fuel type.
+
+        :return: json with all lowest prices data
+        """
+        fuel_objects = []
+        lowest_prices = get_lowest_current_prices()
+
+        for obj in lowest_prices:
+            fuel_objects.append({
+                'fuel_type': obj[0],
+                'price': obj[1][1],
+                'providers': obj[1][0]
+            })
+
+        return fuel_objects
--- app/src/modules/api/namespaces/current.py
+++ app/src/modules/api/namespaces/current.py
@@ -1,24 +1,34 @@
-from flask_restx import Resource, Namespace
+from flask_restx import Resource, Namespace, fields
 
 from src.modules.main.models import FuelPriceModel
 
 api = Namespace('Current Prices')
 
 
+current_sr = api.model('Current', {
+    'provider': fields.String(required=True, description='Provider name'),
+    'name': fields.String(required=True, description="Fuel name"),
+    'type_alt': fields.String(required=True, description="Fuel alternative name"),
+    'price': fields.Float(required=True, description="Fuel price"),
+    'change_rate': fields.Float(required=True, description="Fuel price change rate"),
+    'date': fields.DateTime(required=True, description="Fuel price date"),
+    'last_updated': fields.DateTime(required=True, description="Fuel price last updated"),
+})
+
+
 @api.route("/")
 class Current(Resource):
-    """All current prices API Current endpoint."""
+    """All current prices API endpoint."""
 
+    @api.doc('get_all_current_prices')
+    @api.marshal_list_with(current_sr)
     def get(self):
         """
         Get all current prices.
 
         :return: json with all current prices data
         """
-        from src.modules.api.schemas import FuelPriceSchema
-
-        fuel_objects = FuelPriceModel.read_current_prices()
-        return FuelPriceSchema(many=True).dump(fuel_objects)
+        return FuelPriceModel.read_current_prices().all()
 
 
 @api.route("/<string:provider>")
@@ -27,6 +37,8 @@ class Current(Resource):
 class ProviderCurrent(Resource):
     """Provider current prices API endpoint."""
 
+    @api.doc('get_provider_current_prices')
+    @api.marshal_list_with(current_sr)
     def get(self, provider):
         """
         Get all provider prices.
@@ -34,14 +46,12 @@ class ProviderCurrent(Resource):
         :param provider: fuel provider name
         :return: json with provider current prices data
         """
-        from src.modules.api.schemas import FuelPriceSchema
-
         if provider.lower() not in ['gulf', 'wissol', 'rompetrol', 'socar', 'lukoil']:
-            return 'Provider not found', 404
+            return 'Invalid provider name.', 409
 
         fuel_objects = FuelPriceModel.read_current_prices(
             {
                 'provider': provider.capitalize()
             }
         )
-        return FuelPriceSchema(many=True).dump(fuel_objects)
+        return fuel_objects.all()
