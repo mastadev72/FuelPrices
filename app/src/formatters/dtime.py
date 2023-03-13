b8683806fcc5af49e741eee14e338b0e19270d7c
Nikoloz Naskidashvili <nikoloz2005@gmail.com>
2023-03-13 13:28:44 +0400

    fix: tox errors

--- app/src/formatters/dtime.py
+++ app/src/formatters/dtime.py
@@ -2,6 +2,7 @@ import pytz
 
 
 def datetimefilter(value, dt_format="%H:%M %d-%b-%Y"):
+    """Set date timezone and convert to string according to the format specified."""
     tz = pytz.timezone('Asia/Tbilisi')
     utc = pytz.timezone('UTC')
     value = utc.localize(value, is_dst=None).astimezone(pytz.utc)
